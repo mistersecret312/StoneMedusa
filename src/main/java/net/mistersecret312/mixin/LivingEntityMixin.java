fc24b98526b45dc9633063996039bccdc403e32a
mistersecret312 <sergiigamer7@gmail.com>
2025-02-26 23:36:36 +0200

    Drop loot on break, gem cutter fix

--- src/main/java/net/mistersecret312/mixin/LivingEntityMixin.java
+++ src/main/java/net/mistersecret312/mixin/LivingEntityMixin.java
@@ -5,7 +5,10 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.Cow;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.util.LazyOptional;
+import net.mistersecret312.stonemedusa.capability.PetrifiedCapability;
 import net.mistersecret312.stonemedusa.config.PetrificationConfig;
 import net.mistersecret312.stonemedusa.init.CapabilitiesInit;
 import net.mistersecret312.stonemedusa.init.EffectInit;
@@ -18,6 +21,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Optional;
+
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin
 {
@@ -25,25 +30,31 @@ public class LivingEntityMixin
     public void hurt(DamageSource pSource, float pAmount, CallbackInfoReturnable<Boolean> cir)
     {
         LivingEntity entity = ((LivingEntity) (Object) this);
-        entity.getCapability(CapabilitiesInit.PETRIFIED).ifPresent(cap ->
+        LazyOptional<PetrifiedCapability> cap = entity.getCapability(CapabilitiesInit.PETRIFIED);
+        if(cap.isPresent())
         {
-            if (cap.isPetrified() && PetrificationConfig.petrified_entity_damage.get())
+            Optional<PetrifiedCapability> capOp = cap.resolve();
+            if(capOp.isPresent())
             {
-                cap.setBreakStage(cap.getBreakStage() + (pAmount > 5f ? 1 : 0));
-                if(entity.level().isClientSide)
+                PetrifiedCapability capability = capOp.get();
+                if (capability.isPetrified() && PetrificationConfig.petrified_entity_damage.get())
                 {
-                    NetworkInit.sendToServer(new BreakingEntityPetrifiedPacket(entity.getId()));
+                    capability.setBreakStage(capability.getBreakStage() + (pAmount > 5f ? 1 : 0));
+                    if(entity.level().isClientSide)
+                    {
+                        NetworkInit.sendToServer(new BreakingEntityPetrifiedPacket(entity.getId()));
+                    }
+                    if (capability.getBreakStage() >= 9 && PetrificationConfig.petrified_entity_destroy.get())
+                    {
+                        entity.level().playLocalSound(entity.blockPosition(), SoundEvents.STONE_BREAK, SoundSource.BLOCKS, 1.3f, 1f, false);
+                        entity.level().playSound(entity, entity.blockPosition(), SoundEvents.STONE_BREAK, SoundSource.BLOCKS, 1f, 1f);
+                        entity.level().addDestroyBlockEffect(entity.blockPosition(), Blocks.STONE.defaultBlockState());
+                        pAmount = Float.MAX_VALUE;
+                        return;
+                    }
+                    cir.setReturnValue(false);
                 }
-                if (cap.getBreakStage() >= 9 && PetrificationConfig.petrified_entity_destroy.get())
-                {
-                    entity.level().playLocalSound(entity.blockPosition(), SoundEvents.STONE_BREAK, SoundSource.BLOCKS, 1.3f, 1f, false);
-                    entity.level().playSound(entity, entity.blockPosition(), SoundEvents.STONE_BREAK, SoundSource.BLOCKS, 1f, 1f);
-                    entity.level().addDestroyBlockEffect(entity.blockPosition(), Blocks.STONE.defaultBlockState());
-                    entity.discard();
-                }
-                cir.setReturnValue(false);
             }
-
-        });
+        }
     }
 }
